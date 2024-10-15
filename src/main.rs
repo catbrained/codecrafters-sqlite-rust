685d29fda90aaeecfacd568fcfed8c8cae2044c3
Linda Siemons <linda@catbrained.dev>
2024-10-15 23:24:24 +0200

    Extract .dbinfo logic
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::{bail, Result};
 use std::fs::File;
 use std::io::prelude::*;
 use std::os::unix::fs::FileExt;
+use std::path::Path;
 
 fn main() -> Result<()> {
     // Parse arguments
@@ -16,28 +17,43 @@ fn main() -> Result<()> {
     let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
-            let mut file = File::open(&args[1])?;
-            let mut header = [0; 100];
-            file.read_exact(&mut header)?;
-
-            // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            let page_size = u16::from_be_bytes([header[16], header[17]]);
-
-            // The b-tree page header is 8 bytes for leaf pages,
-            // and 12 bytes for interior pages.
-            let mut b_tree_page_header = [0; 8];
-            // The b-tree page header directly follows the database header.
-            file.read_exact_at(&mut b_tree_page_header, 100)?;
-            let page_type = u8::from_be(b_tree_page_header[0]);
-            // 0x0a: leaf index b-tree page, 0x0d: leaf table b-tree page
-            debug_assert!(page_type == 0x0a || page_type == 0x0d);
-            let num_cells = u16::from_be_bytes([b_tree_page_header[3], b_tree_page_header[4]]);
-
-            println!("database page size: {}", page_size);
-            println!("number of tables: {}", num_cells);
+            let db_info = dot_dbinfo(&args[1])?;
+            println!("database page size: {}", db_info.db_page_size);
+            println!("number of tables: {}", db_info.num_tables);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
 
     Ok(())
+}
+
+struct DbInfo {
+    db_page_size: u16,
+    num_tables: u16,
+}
+
+fn dot_dbinfo(db_file: impl AsRef<Path>) -> Result<DbInfo> {
+    let mut file = File::open(db_file)?;
+    let mut header = [0; 100];
+    file.read_exact(&mut header)?;
+
+    // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
+    let db_page_size = u16::from_be_bytes([header[16], header[17]]);
+
+    // The b-tree page header is 8 bytes for leaf pages,
+    // and 12 bytes for interior pages.
+    let mut b_tree_page_header = [0; 8];
+    // The b-tree page header directly follows the database header.
+    file.read_exact_at(&mut b_tree_page_header, 100)?;
+    let page_type = u8::from_be(b_tree_page_header[0]);
+    // 0x0a: leaf index b-tree page, 0x0d: leaf table b-tree page
+    debug_assert!(page_type == 0x0a || page_type == 0x0d);
+    let num_tables = u16::from_be_bytes([b_tree_page_header[3], b_tree_page_header[4]]);
+
+    let db_info = DbInfo {
+        db_page_size,
+        num_tables,
+    };
+
+    Ok(db_info)
 }
