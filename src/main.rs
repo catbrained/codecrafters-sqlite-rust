74707959bcd72c6d854d24a233876af348e49b07
Linda Siemons <linda@catbrained.dev>
2024-10-15 23:48:16 +0200

    Add some TODOs
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/main.rs
+++ src/main.rs
@@ -42,11 +42,16 @@ fn dot_dbinfo(db_file: impl AsRef<Path>) -> Result<DbInfo> {
 
     // The b-tree page header is 8 bytes for leaf pages,
     // and 12 bytes for interior pages.
+    // TODO: This will break if we are reading a DB file that
+    //       contains more pages (i.e., when this page is _not_ a leaf page).
     let mut b_tree_page_header = [0; 8];
     // The b-tree page header directly follows the database header.
     file.read_exact_at(&mut b_tree_page_header, 100)?;
     let page_type = u8::from_be(b_tree_page_header[0]);
     // 0x0a: leaf index b-tree page, 0x0d: leaf table b-tree page
+    // TODO: Handle other page types. In particular, we need to actually walk the
+    //       b-tree to count the number of tables. Currently we assume that there is
+    //       only this one page.
     debug_assert!(page_type == 0x0a || page_type == 0x0d);
     let num_tables = u16::from_be_bytes([b_tree_page_header[3], b_tree_page_header[4]]);
 
